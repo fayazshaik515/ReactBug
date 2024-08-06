e3c8d2b27009469d20462fb1c186fe2dc3877d82
fayazshaik515 <113083347+fayazshaik515@users.noreply.github.com>
2024-08-06 09:25:44 +0530

    App.js

---
+++ App.js
@@ -0,0 +1,10 @@
+  import ButtonAppBar from "./component/Appbar";
+  function App() {
+    return (
+      <div className="App">
+        <ButtonAppBar/>
+      </div>
+    );
+  }
+
+  export default App;
